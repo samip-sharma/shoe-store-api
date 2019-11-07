91bf06ae12a5a9a6e58beaa08755e12b65bf4488
samip-sharma <samip.sharma963@gmail.com>
2019-11-07 15:53:28 -0500

    adds image to shoe table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_203848) do
+ActiveRecord::Schema.define(version: 2019_11_07_205232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_11_07_203848) do
     t.boolean "latest", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "user_shoes", force: :cascade do |t|
